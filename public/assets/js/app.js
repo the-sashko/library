f4a41fd30ae3fd075bd963814cc3105bb4a1412c
The Sashko <sashko.me>
2021-03-17 13:12:22 +0200

    improve assets

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -3,11 +3,58 @@ let App = {
         App.initFileManagerView();
     },
 
+    init: function() {
+        App.initForm();
+    },
+
     initFileManagerView: function() {
+        if (document.getElementById('greed-view-button') != null) {
+            App.initFileManagerGreedView();
+        }
+
+        if (document.getElementById('list-view-button') != null) {
+            App.initFileManagerListView();
+        }
+    },
+
+    initForm: function() {
+        if (document.getElementById('file-form') != null) {
+            App.initFileForm();
+        }
+    },
+
+    initFileForm: function() {
+        document.getElementById('file-form-input-file').onchange = function() {
+            let inputValue = document.getElementById('file-form-input-file').value;
+
+            inputValue = inputValue.split('/');
+            inputValue = inputValue[inputValue.length - 1];
+
+            inputValue = inputValue.split('\\');
+            inputValue = inputValue[inputValue.length - 1];
+
+            if (inputValue.length > 26) {
+                inputValue = inputValue.substring(0, 13) +
+                             '…' +
+                             inputValue.substring(inputValue.length - 12);
+            }
+
+            inputValue = inputValue.toLowerCase();
+
+            document.getElementById('form-input-file-name').innerHTML = '<i class="fa fa-link"></i> ' +
+                                                                        inputValue;
+
+            document.getElementById('form-input-file-name').style.display = 'block';
+        };
+    },
+
+    initFileManagerGreedView: function() {
         document.getElementById('greed-view-button').onclick = function() {
             App.greedView();
         };
+    },
 
+    initFileManagerListView: function() {
         document.getElementById('list-view-button').onclick = function() {
             App.listView();
         };
